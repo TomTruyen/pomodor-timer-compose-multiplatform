f96af8ffb69b45f263a93a71e1d14d41230ce64a
Tom Truyen <tom.truyen@gmail.com>
2024-08-17 18:04:48 +0200

    chore: remove unused comments

--- iosApp/src/ViewController.kt
+++ iosApp/src/ViewController.kt
@@ -4,12 +4,10 @@ import com.mmk.kmpnotifier.notification.NotifierManager
 import com.mmk.kmpnotifier.notification.configuration.NotificationPlatformConfiguration
 
 fun ViewController() = ComposeUIViewController {
-    // TODO: Test Notifications. Might have to be initialized at another place
     NotifierManager.initialize(
         configuration = NotificationPlatformConfiguration.Ios(
             showPushNotification = true,
             askNotificationPermissionOnStart = true,
-//            notificationSoundName = "TO BE DETERMINED"
         )
     )
 
